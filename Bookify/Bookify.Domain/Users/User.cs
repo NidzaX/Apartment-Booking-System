9fa9204ed65b36f50952eda74429a7d810f734b6
NidzaX <59400424+NidzaX@users.noreply.github.com>
2024-11-27 19:16:05 +0100

    Adding the Domain Event Pattern

--- Bookify/Bookify.Domain/Users/User.cs
+++ Bookify/Bookify.Domain/Users/User.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Users.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,8 @@ namespace Bookify.Domain.Users
         public static User Create(FirstName firstName, LastName lastName, Email email)
         {
             var user = new User(Guid.NewGuid(), firstName, lastName, email);
+
+            user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
 
             return user;
         }
